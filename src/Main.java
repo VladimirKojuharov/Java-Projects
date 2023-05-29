bfbf123511e1f6bfdd536b5c297b666a2edd687f
VladimirKojuharov <129941262+VladimirKojuharov@users.noreply.github.com>
2023-05-29 14:59:08 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+//         MAKE YOUR CONSOLE TO 17 ROWS.
+
+
+
+
+        StartGame LETS_PLAY = new StartGame();
+
+        LETS_PLAY.runGame();
+
+
+
+
+
+
+
+    }
+}
